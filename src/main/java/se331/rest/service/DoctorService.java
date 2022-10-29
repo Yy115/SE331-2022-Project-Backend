cb159f16ae0f65b96520dff38dd44d601deb0d2c
Chen Yang <yc2261124363@outlook.com>
2022-10-29 15:31:31 +0800

    add service files

--- src/main/java/se331/rest/service/DoctorService.java
+++ src/main/java/se331/rest/service/DoctorService.java
@@ -1,4 +1,12 @@
 package se331.rest.service;
 
-public class DoctorService {
+
+import org.springframework.data.domain.Page;
+import se331.rest.entity.Doctor;
+
+public interface DoctorService {
+    Integer getDoctorSize();
+    Page<Doctor> getDoctors(Integer pageSize, Integer page);
+    Doctor getDoctor(Long id);
+    Doctor save(Doctor doctor);
 }
